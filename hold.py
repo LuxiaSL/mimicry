957e69addea7b0e9129108cac3c51446f21ffa43
Luxia <nebulagaming123@gmail.com>
2023-06-24 20:56:45 -0400

    Update hold.py

--- hold.py
+++ hold.py
@@ -1,8 +1,3 @@
-# todo:
-#  stats on most called
-#  fix sorts (sorting alphabetically, not numerically), catch errors on delete and select
-
-
 import json
 import tkinter as tk
 from tkinter import messagebox
@@ -43,17 +38,31 @@ class SortableTable(ttk.Treeview):
     def __init__(self, parent=None, columns=None, **kwargs):
         ttk.Treeview.__init__(self, parent, columns=columns, **kwargs)
         self.sort_direction = [1] * len(columns)
+
+        # Set the column widths here. Adjust these values as needed.
+        self.column("Name", width=175)
+        self.column("Cap size", width=55)
+        self.column("Rate", width=75)
+        self.column("Amount", width=90)
+        self.column("Stacks", width=75)
+
         for i, col in enumerate(columns, 1):
             self.heading(col, text=col, command=lambda _col=col, _i=i: self.sort_column(_col, _i))
 
     def sort_column(self, col, i):
         data = [(self.set(child, col), child) for child in self.get_children('')]
-        data.sort(reverse=self.sort_direction[i - 1] == 1)
-
+        # Try to convert to float for comparison. If it fails, it's a string, so compare as is.
+        data.sort(key=lambda t: (self.try_parse_float(t[0]), t[1]), reverse=self.sort_direction[i - 1] == 1)
         for indx, item in enumerate(data):
             self.move(item[1], '', indx)
-
         self.sort_direction[i - 1] *= -1
+
+    @staticmethod
+    def try_parse_float(value):
+        try:
+            return float(value)
+        except ValueError:
+            return value
 
 
 class Application(tk.Frame):
@@ -61,12 +70,16 @@ class Application(tk.Frame):
         super().__init__(master, bg='lightgray')
         self.master = master
         self.grid(padx=15, pady=15)
+
+        # Load the stats database from the JSON file
+        self.stats_data = self.load_stats_data()
+
         self.create_widgets()
 
     def create_widgets(self):
         self.item_list = []
         self.default_font = font.nametofont("TkDefaultFont")
-        self.default_font.configure(size=14, weight='bold')
+        self.default_font.configure(size=12, weight='bold')
 
         # Load item names from JSON file
         with open('items.json', 'r') as f:
@@ -92,7 +105,7 @@ class Application(tk.Frame):
         self.calculate_button.grid(row=0, column=4, padx=5, pady=10)
 
         # Items table
-        self.items_table = SortableTable(self, columns=('Name', 'Cap size', 'Rate', 'Amount', 'Stacks'), show='headings')
+        self.items_table = SortableTable(self, columns=('Name', 'Cap size', 'Rate', 'Amount', 'Stacks'), show='headings', height=16)
         self.items_table.grid(row=1, column=0, columnspan=5, sticky='nsew')
 
         # Search box
@@ -127,16 +140,38 @@ class Application(tk.Frame):
     def update_listbox(self, search_var):
         search_term = search_var.get().lower()
         self.search_results.delete(0, tk.END)
-        matches = []
-        for item in self.item_data:
+
+        # Create a list of tuples containing the item name, stats count, and matching character percentage
+        items_with_stats = [(item, self.stats_data.get(item, 0), self.calculate_matching_percentage(search_term, item))
+                            for item in self.item_data]
+
+        # Sort the items based on the stats count in descending order and the matching character percentage in descending order
+        sorted_items = sorted(items_with_stats, key=lambda x: (x[1], x[2]), reverse=True)
+
+        # Iterate through the sorted items and add them to the search results listbox
+        for item, _, _ in sorted_items:
             if search_term in item.lower():
-                matches.append(item)
-        for item in matches[:5]:  # Show only the top 5 matches
-            self.search_results.insert(tk.END, item)
+                self.search_results.insert(tk.END, item)
+
+    def calculate_matching_percentage(self, search_term, item):
+        common_length = 0
+        min_length = min(len(search_term), len(item))
+        for i in range(min_length):
+            if search_term[i] == item[i]:
+                common_length += 1
+            else:
+                break
+        if min_length > 0:
+            return common_length / min_length
+        else:
+            return 0.0
 
     def on_item_selected(self, event):
-        selected_item = self.search_results.get(self.search_results.curselection())
-        self.search_var.set(selected_item)
+        try:
+            selected_item = self.search_results.get(self.search_results.curselection())
+            self.search_var.set(selected_item)
+        except tk.TclError:
+            pass  # Invalid listbox index, do nothing
 
     def add_item(self):
         name = self.search_var.get()
@@ -145,6 +180,10 @@ class Application(tk.Frame):
             self.item_list.append({'name': name, 'rate': rate, 'cap': self.item_data[name]})
             item = self.item_list[-1]
             self.items_table.insert('', 'end', values=(item['name'], item['cap'], item['rate'], '', ''))
+
+            # Increment the stats count for the item
+            self.increment_stats_count(name)
+
             self.calculate()  # Update the calculation
         else:
             should_add = messagebox.askyesno("Question", "Item not found in the database. Would you like to add it?")
@@ -160,9 +199,47 @@ class Application(tk.Frame):
                 self.calculate()  # Update the calculation
 
     def remove_item(self):
-        selected_item = self.items_table.selection()[0]
-        self.items_table.delete(selected_item)
-        del self.item_list[int(selected_item)]
+        try:
+            selected_item = self.items_table.selection()[0]
+            item_name = self.items_table.item(selected_item)['values'][0]
+
+            # Decrement the stats count for the item
+            self.decrement_stats_count(item_name)
+
+            self.items_table.delete(selected_item)
+            del self.item_list[int(selected_item)]
+        except IndexError:
+            # No item selected, do nothing
+            return
+
+    def decrement_stats_count(self, item_name):
+        if item_name in self.stats_data:
+            self.stats_data[item_name] -= 1
+            if self.stats_data[item_name] == 0:
+                del self.stats_data[item_name]
+
+        # Save the updated stats data to the JSON file
+        self.save_stats_data()
+
+    def increment_stats_count(self, item_name):
+        if item_name in self.stats_data:
+            self.stats_data[item_name] += 1
+        else:
+            self.stats_data[item_name] = 1
+
+        # Save the updated stats data to the JSON file
+        self.save_stats_data()
+
+    def load_stats_data(self):
+        try:
+            with open('stats.json', 'r') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return {}
+
+    def save_stats_data(self):
+        with open('stats.json', 'w') as f:
+            json.dump(self.stats_data, f)
 
     def calculate(self):
         if not self.cargo_size_entry.get().isdigit():
@@ -182,6 +259,6 @@ class Application(tk.Frame):
 
 
 root = tk.Tk()
-root.geometry('830x550')
+root.geometry('505x625')
 app = Application(master=root)
 app.mainloop()
